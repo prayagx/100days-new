9540c54f243cec8b087d833b3d9779101c4e4154
Prayag Gupta <guptaprayag16@gmail.com>
2023-08-31 22:06:40 +0530

    project3

---
+++ project3/app.js
@@ -0,0 +1,136 @@
+const quizData = [{
+    question: "Which of the following is a client site language?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+},
+{
+    question: "What does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+},
+{
+    question: "What does CSS stands for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+},
+
+{
+    question: "What does the acronym 'DOM' stand for in the context of web development?",
+    a: "Document Object Model",
+    b: "Data Orientation Module",
+    c: "Dynamic Ordinal Method",
+    d: "Digital Output Mode",
+    correct: "a",
+
+},
+
+{
+    question: "Which keyword is used to declare a variable in JavaScript?",
+    a: "new",
+    b: "variable",
+    c: "let",
+    d: "declare",
+    correct: "c",
+
+},
+
+{
+    question: "What is the purpose of the 'box-sizing' property in CSS?",
+    a: "To control the spacing between elements",
+    b: "To adjust the opacity of an element",
+    c: "To control the box model used for sizing elements",
+    d: "To set the background image of an element",
+    correct: "c",
+
+},
+
+{
+    question: "Which property is used in CSS to change the text color of an element?",
+    a: "font-color",
+    b: "text-style",
+    c: "color",
+    d: "text-color",
+    correct: "c",
+
+}
+];
+let index = 0;
+let correct = 0,
+incorrect = 0,
+total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+if (total === index) {
+    return quizEnd()
+}
+reset()
+const data = quizData[index]
+questionBox.innerHTML = `${index + 1}) ${data.question}`
+allInputs[0].nextElementSibling.innerText = data.a
+allInputs[1].nextElementSibling.innerText = data.b
+allInputs[2].nextElementSibling.innerText = data.c
+allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener(
+"click",
+function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+    index++;
+    loadQuestion()
+}
+)
+
+const getAnswer = () => {
+let ans;
+allInputs.forEach(
+    (inputEl) => {
+        if (inputEl.checked) {
+            ans = inputEl.value;
+        }
+    }
+)
+return ans;
+}
+
+const reset = () => {
+allInputs.forEach(
+    (inputEl) => {
+        inputEl.checked = false;
+    }
+)
+}
+
+const quizEnd = () => {
+// console.log(document.getElementsByClassName("container"));
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+    </div>
+`
+}
+loadQuestion(index);
