ce4da2c168c3c8cdefe90f0c51dbf843545953b8
Prayag Gupta <guptaprayag16@gmail.com>
2023-09-04 16:25:16 +0530

    work

--- shopping-cart/images/main.js
+++ shopping-cart/images/main.js
@@ -26,73 +26,42 @@ let shopitemsdata = [{
     price: 100,
     desc: "lorem ipsum dolor sit amet, consectetur adip",
     img: "images/img-4.jpg",
-}]
+}];
 
-let generateshop = () => {
-    return shop.innerHTML = shopitemsdata.map((item) => {
-      return `
-        <div class="item">
-          <img width="219" src="images/img-1.jpg" alt="">
+function generateshop() {
+    return shop.innerHTML = shopitemsdata.map((x) => {
+        let { name, price, img, desc, id } = x;
+        return `
+        <div id=product-id-${id} class="item">
+          <img width="219" src=${img} alt="">
           <div class="details">
-            <h3>Casual Shirt</h3>
-            <p> Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
+            <h3> ${name}</h3>
+            <p> ${desc}</p>
             <div class="price-quantity">
-              <h2>$45</h2>
+              <h2>$ ${price}</h2>
               <div class="buttons">
-                <i class="bi bi-plus-square"></i>
+                <i onclick ="increment(${id})" class="bi bi-plus-square"></i>
                 <div class="quantity">0</div>
-                <i class="bi bi-dash-square"></i>
+                <i onclick ="decrement(${id})" class="bi bi-dash-square"></i>
               </div>
             </div>
           </div>
         </div>
-        <div class="item">
-          <img width="219" src="images/img-2.jpg" alt="">
-          <div class="details">
-            <h3>Office Shirt</h3>
-            <p> Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-            <div class="price-quantity">
-              <h2>$70</h2>
-              <div class="buttons">
-                <i class="bi bi-plus-square"></i>
-                <div class="quantity">0</div>
-                <i class="bi bi-dash-square"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img width="219" src="images/img-3.jpg" alt="">
-          <div class="details">
-            <h3>T-Shirt</h3>
-            <p> Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-            <div class="price-quantity">
-              <h2>$25</h2>
-              <div class="buttons">
-                <i class="bi bi-plus-square"></i>
-                <div class="quantity">0</div>
-                <i class="bi bi-dash-square"></i>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="item">
-          <img width="219" src="images/img-4.jpg" alt="">
-          <div class="details">
-            <h3>Men's Suit</h3>
-            <p> Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-            <div class="price-quantity">
-              <h2>$100</h2>
-              <div class="buttons">
-                <i class="bi bi-plus-square"></i>
-                <div class="quantity">0</div>
-                <i class="bi bi-dash-square"></i>
-              </div>
-            </div>
-          </div>
-        </div>
+        
       `;
     }).join("");
-  }
+}
 
 generateshop();
+
+let increment = (id) => {
+    let selecteditem = id;
+    console.log(selecteditem);
+};
+
+let decrement = (id) => {
+    let selecteditem = id;
+    console.log(selecteditem);
+};
+
+let update = () => {};
